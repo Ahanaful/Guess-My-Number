ea8662d404b6f5403ee1f9f05fa3a83b9308a404
Ahanaful <75117798+Ahanaful@users.noreply.github.com>
2022-02-04 17:18:11 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+
+
+int main()
+{ srand(time(0));
+int theNumber = rand() % 100+1;
+int tries = 0, guess;
+    cout << "\n Welcome to the Guess Number \n" ;
+
+    do {
+        cout << " Enter your guess = " ;
+        cin >> guess;
+        ++tries;
+
+        if (guess > theNumber){
+
+            cout << " You are too high" << endl;
+
+        }
+
+        if ( guess < theNumber){
+
+            cout << "You are too low" << endl;
+
+        }
+    }
+        while (guess != theNumber);
+
+        cout << " you have guessed right" << " at the " << tries << "th time" <<endl;
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
